bb673dead4204955ba1f00896ae4dc9cc54c815e
Jonathan Carifio <carifio24@gmail.com>
2023-05-12 00:23:20 -0400

    Add button to import character in selection controller (not yet functional).

--- Spellbook/CharacterSelectionController.swift
+++ Spellbook/CharacterSelectionController.swift
@@ -14,6 +14,7 @@ class CharacterSelectionController: UIViewController, UITableViewDelegate, UITab
     @IBOutlet weak var selectionTitle: UILabel!
     @IBOutlet weak var selectionMessage: UILabel!
     @IBOutlet weak var newCharacterButton: UIButton!
+    @IBOutlet weak var importCharacterButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var backgroundView: UIImageView!
     
