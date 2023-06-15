e74c48ec6c533d6f4baff4a2e1ee0782f2e73f60
Jonathan Carifio <carifio24@gmail.com>
2023-06-15 11:37:41 +0000

    Finish first character profile parsing test.

--- SpellbookTests/CharacterProfileTests.swift
+++ SpellbookTests/CharacterProfileTests.swift
@@ -12,7 +12,7 @@ import XCTest
 
 class CharacterProfileTests: XCTestCase {
     func testV3_0_0_loadFromJSON() {
-        let jsonString = "{\"VersionCode\":\"3.0.0\",\"SpellSlotStatus\":{\",\"\"usedSlotsKey\":[2,0,0,0,0,0,0,0,0],\"totalSlots\":[7,0,0,0,0,0,0,0,0]},\"CharacterName\":\"test\",\"SpellFilterStatus\":{\"spells\":[{\"favorite\":true,\"spellID\":250,\"prepared\":false,\"known\":false},{\"favorite\":true,\"prepared\":true,\"spellID\":6,\"known\":true},{\"known\":false,\"prepared\":true,\"favorite\":false,\"spellID\":351},{\"prepared\":true,\"favorite\":true,\"known\":false,\"spellID\":272},{\"spellID\":215,\"favorite\":false,\"prepared\":false,\"known\":true},{\"prepared\":false,\"favorite\":true,\"known\":true,\"spellID\":352},{\"spellID\":299,\"prepared\":true,\"favorite\":false,\"known\":true},{\"prepared\":false,\"spellID\":364,\"known\":true,\"favorite\":true}]},\"SortFilterStatus\":{\"StatusFilter\":\"Favorites\",\"NotRitual\":false,\"RangeBounds\":{\"MaxValue\":1,\"MaxUnit\":\"mile\",\"MinUnit\":\"foot\",\"MinValue\":0},\"MinSpellLevel\":2,\"Ritual\":true,\"CastingTimeTypes\":[\"1 action\",\"1 reaction\",\"Other\"],\"DurationBounds\":{\"MaxValue\":30,\"MinUnit\":\"round\",\"MinValue\":0,\"MaxUnit\":\"hour\"},\"MaxSpellLevel\":8,\"CastingTimeBounds\":{\"MinUnit\":\"second\",\"MinValue\":2,\"MaxValue\":22,\"MaxUnit\":\"hour\"},\"Classes\":[\"Artificer\",\"Druid\",\"Paladin\",\"Ranger\",\"Sorcerer\",\"Warlock\",\"Wizard\"],\"UseTCEExpandedLists\":true,\"DurationTypes\":[\"Special\",\"Finite duration\",\"Until dispelled\"],\"ApplyFiltersToSpellLists\":true,\"Sourcebooks\":[\"phb\",\"ai\",\"aag\"],\"Reverse1\":false,\"Concentration\":true,\"NotComponentsFilters\":[true,true,false],\"ComponentsFilters\":[false,true,true],\"Schools\":[\"Conjuration\",\"Divination\",\"Enchantment\",\"Evocation\",\"Illusion\",\"Transmutation\"],\"SortField1\":\"Level\",\"NotConcentration\":true,\"SortField2\":\"Range\",\"RangeTypes\":[\"Special\",\"Sight\",\"Ranged\",\"Unlimited\"],\"ApplyFiltersToSearch\":false,\"Reverse2\":true}}"
+        let jsonString = "{\"VersionCode\":\"3.0.0\",\"SpellFilterStatus\":{\"spells\":[{\"known\":false,\"favorite\":true,\"spellID\":299,\"prepared\":true},{\"known\":false,\"favorite\":false,\"prepared\":true,\"spellID\":1},{\"spellID\":363,\"prepared\":true,\"favorite\":false,\"known\":false},{\"known\":true,\"favorite\":false,\"spellID\":215,\"prepared\":true},{\"spellID\":6,\"known\":false,\"prepared\":true,\"favorite\":false},{\"spellID\":362,\"favorite\":true,\"known\":true,\"prepared\":true},{\"known\":true,\"spellID\":250,\"prepared\":false,\"favorite\":true},{\"prepared\":true,\"favorite\":false,\"known\":true,\"spellID\":351}]},\"SpellSlotStatus\":{\"usedSlotsKey\":[2,1,1,0,0,0,0,0,0],\"totalSlots\":[7,2,1,0,0,0,0,0,0]},\"SortFilterStatus\":{\"RangeTypes\":[\"Special\",\"Sight\",\"Ranged\",\"Unlimited\"],\"ApplyFiltersToSearch\":false,\"Sourcebooks\":[\"phb\",\"ai\",\"aag\"],\"UseTCEExpandedLists\":true,\"MaxSpellLevel\":8,\"CastingTimeBounds\":{\"MaxUnit\":\"hour\",\"MaxValue\":22,\"MinUnit\":\"second\",\"MinValue\":2},\"Reverse1\":false,\"NotRitual\":false,\"NotComponentsFilters\":[true,true,false],\"NotConcentration\":true,\"Schools\":[\"Conjuration\",\"Divination\",\"Enchantment\",\"Illusion\",\"Transmutation\"],\"Classes\":[\"Artificer\",\"Druid\",\"Paladin\",\"Ranger\",\"Sorcerer\",\"Warlock\",\"Wizard\"],\"SortField2\":\"Range\",\"MinSpellLevel\":2,\"StatusFilter\":\"Favorites\",\"Ritual\":true,\"ComponentsFilters\":[false,true,true],\"Reverse2\":true,\"CastingTimeTypes\":[\"1 action\",\"1 reaction\",\"Other\"],\"DurationBounds\":{\"MinUnit\":\"round\",\"MaxUnit\":\"hour\",\"MaxValue\":30,\"MinValue\":6},\"RangeBounds\":{\"MinValue\":0,\"MaxValue\":1,\"MaxUnit\":\"mile\",\"MinUnit\":\"foot\"},\"ApplyFiltersToSpellLists\":true,\"SortField1\":\"Level\",\"DurationTypes\":[\"Special\",\"Finite duration\",\"Until dispelled\"],\"Concentration\":true},\"CharacterName\":\"test\"}"
         
         let sion = SION(json: jsonString)
         guard let profile = try? CharacterProfile.fromSION(sion) else {
@@ -76,7 +76,7 @@ class CharacterProfileTests: XCTestCase {
         XCTAssertEqual(castingTimeBounds.3, TimeUnit.hour)
 
         let durationBounds = sortFilterStatus.getDurationBounds()
-        XCTAssertEqual(durationBounds.0, 0)
+        XCTAssertEqual(durationBounds.0, 6)
         XCTAssertEqual(durationBounds.1, TimeUnit.round)
         XCTAssertEqual(durationBounds.2, 30)
         XCTAssertEqual(durationBounds.3, TimeUnit.hour)
@@ -87,17 +87,27 @@ class CharacterProfileTests: XCTestCase {
         XCTAssertEqual(rangeBounds.2, 1)
         XCTAssertEqual(rangeBounds.3, LengthUnit.mile)
 
-        // {\"spells\":[{\"favorite\":true,\"spellID\":250,\"prepared\":false,\"known\":false},{\"favorite\":true,\"prepared\":true,\"spellID\":6,\"known\":true},{\"known\":false,\"prepared\":true,\"favorite\":false,\"spellID\":351},{\"prepared\":true,\"favorite\":true,\"known\":false,\"spellID\":272},{\"spellID\":215,\"favorite\":false,\"prepared\":false,\"known\":true},{\"prepared\":false,\"favorite\":true,\"known\":true,\"spellID\":352},{\"spellID\":299,\"prepared\":true,\"favorite\":false,\"known\":true},{\"prepared\":false,\"spellID\":364,\"known\":true,\"favorite\":true}]}
-        let favoriteSpellIDs = [6, 250, 272, 352, 364]
-        let preparedSpellIDs = [6, 272, 299, 351]
-        let knownSpellIDs = [6, 215, 299, 352, 364]
-        let oneTrueSpellIDs = [6, 215, 250, 272, 299, 351, 352, 364]
+        
+        let favoriteSpellIDs = [250, 299, 362]
+        let preparedSpellIDs = [1, 6, 215, 299, 351, 362, 363]
+        let knownSpellIDs = [215, 250, 351, 362]
+        let oneTrueSpellIDs = [1, 6, 215, 250, 299, 351, 362, 363]
         XCTAssertEqual(spellFilterStatus.favoriteSpellIDs().sorted(), favoriteSpellIDs)
         XCTAssertEqual(spellFilterStatus.preparedSpellIDs().sorted(), preparedSpellIDs)
         XCTAssertEqual(spellFilterStatus.knownSpellIDs().sorted(), knownSpellIDs)
         XCTAssertEqual(spellFilterStatus.spellsWithOneProperty().sorted(), oneTrueSpellIDs)
-        
-        for level in 1...Spellbook.MAX_SPELL_LEVEL {
+
+
+        XCTAssertEqual(spellSlotsStatus.getTotalSlots(level: 1), 7)
+        XCTAssertEqual(spellSlotsStatus.getUsedSlots(level: 1), 2)
+        XCTAssertEqual(spellSlotsStatus.getAvailableSlots(level: 1), 5)
+        XCTAssertEqual(spellSlotsStatus.getTotalSlots(level: 2), 2)
+        XCTAssertEqual(spellSlotsStatus.getUsedSlots(level: 2), 1)
+        XCTAssertEqual(spellSlotsStatus.getAvailableSlots(level: 2), 1)
+        XCTAssertEqual(spellSlotsStatus.getTotalSlots(level: 3), 1)
+        XCTAssertEqual(spellSlotsStatus.getUsedSlots(level: 3), 1)
+        XCTAssertEqual(spellSlotsStatus.getAvailableSlots(level: 3), 0)
+        for level in 4...Spellbook.MAX_SPELL_LEVEL {
             XCTAssertEqual(spellSlotsStatus.getAvailableSlots(level: level), 0)
             XCTAssertEqual(spellSlotsStatus.getUsedSlots(level: level), 0)
             XCTAssertEqual(spellSlotsStatus.getTotalSlots(level: level), 0)
