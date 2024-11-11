2f7107548c81737d7f26ad00aa7581eaf8f5c34a
Carifio24 <carifio24@gmail.com>
2024-11-11 01:46:56 -0500

    Update app serialization utils to account for created spells.

--- Spellbook/SerializationUtils.swift
+++ Spellbook/SerializationUtils.swift
@@ -11,12 +11,14 @@ import Foundation
 class SerializationUtils: NSObject {
     static let profilesDirectoryName = "Characters"
     static let profilesExtension = ".json"
+    static let createdSpellsDirectoryName = "CreatedSpells"
+    static let createdSpellsExtension = ".json"
     static let settingsFile = "Settings.json"
     
     static let documentsDirectory: URL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
     
-    static let profilesDirectory: URL = {
-        let dir = documentsDirectory.appendingPathComponent(profilesDirectoryName)
+    static func getDirectory(directoryName: String) -> URL {
+        let dir = documentsDirectory.appendingPathComponent(directoryName)
         let fileManager = FileManager.default
         if !fileManager.fileExists(atPath: dir.path) {
             do {
@@ -26,15 +28,30 @@ class SerializationUtils: NSObject {
             }
         }
         return dir
-    }()
+    }
+    
+    static let profilesDirectory = getDirectory(directoryName: profilesDirectoryName)
+    static let createdSpellsDirectory = getDirectory(directoryName: createdSpellsDirectoryName)
+    
+    private static func nameFileLocation(name: String, ext: String, directory: URL) -> URL {
+        let filename = name + ext
+        return directory.appendingPathComponent(filename)
+    }
     
     static func profileLocation(name: String) -> URL {
-        let charFile = name + profilesExtension
-        return profilesDirectory.appendingPathComponent(charFile)
+        return nameFileLocation(name: name, ext: profilesExtension, directory: profilesDirectory)
     }
     
     static func profileLocation(profile: CharacterProfile) -> URL {
         return profileLocation(name: profile.name)
+    }
+    
+    static func createdSpellLocation(name: String) -> URL {
+        return nameFileLocation(name: name, ext: createdSpellsExtension, directory: createdSpellsDirectory)
+    }
+    
+    static func createdSpellLocation(spell: Spell) -> URL {
+        return createdSpellLocation(name: spell.name)
     }
     
     static func saveCharacterProfile(profile: CharacterProfile) -> Bool {
@@ -42,35 +59,49 @@ class SerializationUtils: NSObject {
         return profile.save(filename: location)
     }
     
-    static func loadCharacterProfile(name: String) throws -> CharacterProfile {
-        let location = profileLocation(name: name)
-        if var profileText = try? String(contentsOf: location) {
+    static func loadItemFromJSON<T, E: Error>(name: String,
+                                    locationGetter: (String) -> URL,
+                                    itemCreator: (SION) throws -> T,
+                                    errorCreator: () -> E
+    ) throws -> T {
+        let location = locationGetter(name)
+        if var jsonText = try? String(contentsOf: location) {
             do {
-                fixEscapeCharacters(&profileText)
-                let profileSION = SION(json: profileText)
-                let profile = try CharacterProfile.fromSION(profileSION)
-                return profile
+                fixEscapeCharacters(&jsonText)
+                let sion = SION(json: jsonText)
+                let item = try itemCreator(sion)
+                return item
             } catch _ {
-                throw SpellbookError.BadCharacterProfileError
+                throw errorCreator()
             }
         } else {
-            print("Error reading profile")
-            let error = SpellbookError.BadCharacterProfileError
-            print(error.description)
-            throw error
+            throw errorCreator()
         }
     }
     
-    static func deleteCharacterProfile(profile: CharacterProfile) -> Bool {
-        let location = profileLocation(profile: profile)
+    static func loadCharacterProfile(name: String) throws -> CharacterProfile {
+        return try loadItemFromJSON(name: name,
+                                locationGetter: self.profileLocation,
+                                itemCreator: CharacterProfile.fromSION,
+                                errorCreator: { return SpellbookError.BadCharacterProfileError })
+    }
+    
+    private static func deleteFile(location: URL) -> Bool {
         let fileManager = FileManager.default
         do {
             try fileManager.removeItem(at: location)
             return true
         } catch let e {
-            print("\(e)")
             return false
         }
+    }
+    
+    static func deleteCharacterProfile(profile: CharacterProfile) -> Bool {
+        return deleteFile(location: profileLocation(profile: profile))
+    }
+    
+    static func deleteCreatedSpell(spell: Spell) -> Bool {
+        return deleteFile(location: createdSpellLocation(spell: spell))
     }
     
     static func loadSettings() -> Settings {
@@ -91,23 +122,31 @@ class SerializationUtils: NSObject {
         settings.save(filename: settingsLocation)
     }
     
-    static func characterNameList() -> [String] {
-        var charNames: [String] = []
+    static func itemNameList(directory: URL, ext: String) -> [String] {
+        var names: [String] = []
         let fileManager = FileManager.default
-        let extLen = profilesExtension.count
-        guard let enumerator = fileManager.enumerator(at: profilesDirectory, includingPropertiesForKeys: nil) else {
+        let extLen = ext.count
+        guard let enumerator = fileManager.enumerator(at: directory, includingPropertiesForKeys: nil) else {
             return []
         }
         for path in enumerator.allObjects {
             let url = path as! URL
             let element = url.lastPathComponent
-            if (element.hasSuffix(profilesExtension)) {
+            if (element.hasSuffix(ext)) {
                 var name = element
                 name.removeLast(extLen)
-                charNames.append(name)
+                names.append(name)
             }
         }
-        charNames.sort(by: { $0.lowercased() < $1.lowercased() })
-        return charNames
+        names.sort(by: { $0.lowercased() < $1.lowercased() })
+        return names
+    }
+    
+    static func characterNameList() -> [String] {
+        return itemNameList(directory: profilesDirectory, ext: profilesExtension)
+    }
+    
+    static func createdSpellNameList() -> [String] {
+        return itemNameList(directory: createdSpellsDirectory, ext: createdSpellsExtension)
     }
 }
