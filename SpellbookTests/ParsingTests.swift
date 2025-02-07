cf5f3f2d44ad84633e59861a0e1ea340a6072ea7
Carifio24 <carifio24@gmail.com>
2025-02-07 01:29:35 -0500

    Add test of spell list count.

---
+++ SpellbookTests/ParsingTests.swift
@@ -0,0 +1,18 @@
+//
+//  ParsingTests.swift
+//  SpellbookTests
+//
+//  Created by Jonathan Carifio on 2/7/25.
+//  Copyright © 2025 Jonathan Carifio. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Spellbook
+
+class ParsingTests: XCTestCase {
+    func testParseSpellList() {
+        let state = SpellbookAppState()
+        XCTAssert(state.spellList.count == 919)
+    }
+}
