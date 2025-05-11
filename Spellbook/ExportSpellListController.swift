e95b4df5a1c7a27101c29290b22e62912155e498
Carifio24 <carifio24@gmail.com>
2025-05-11 14:53:10 -0400

    WIP: Start working on creating dialog for exporting spell lists.

--- Spellbook/ExportSpellListController.swift
+++ Spellbook/ExportSpellListController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
+enum ExportFormat: Int, CaseIterable {
+    case PDF, Markdown, HTML
+    
+    var fileExtension: String {
+        switch (self) {
+        case .PDF:
+            return "pdf"
+        case .Markdown:
+            return "md"
+        case .HTML:
+            return "html"
+        }
+    }
+    
+    var name: String {
+        switch (self) {
+        case .PDF:
+            return "PDF"
+        case .Markdown:
+            return "Markdown"
+        case .HTML:
+            return "HTML"
+        }
+    }
+    
+    var mimeType: String {
+        switch (self) {
+        case .PDF:
+            return "application/pdf"
+        case .Markdown:
+            return "text/markdown"
+        case .HTML:
+            return "text/html"
+        }
+    }
+}
+
 class ExportSpellListController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var listLabel: UILabel!
+    @IBOutlet weak var formatLabel: UILabel!
+    @IBOutlet weak var allContentLabel: UILabel!
+    @IBOutlet weak var listChoice: UITextField!
+    @IBOutlet weak var formatChoice: UITextField!
+    @IBOutlet weak var allContentSwitch: UISwitch!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var exportButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        cancelButton.addTarget(self, action: #selector(cancelButtonPressed), for: UIControl.Event.touchUpInside)
+        
+        let listChoiceDelegate = TextF
+    }
+    
+    @objc func cancelButtonPressed() {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
